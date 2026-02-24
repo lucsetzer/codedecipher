4069f81d42e7daff59eac3b74953be698da161ef
Luc Setzer <lucsetzer@gmail.com>
2026-02-24 11:12:09 -0500

    Fix waitlist to use correct template

--- routes/waitlist.py
+++ routes/waitlist.py
@@ -65,7 +65,7 @@ async def pro_waitlist_signup(request: Request, email: str = Form(...)):
     
     await conn.close()
     
-    return templates.TemplateResponse("check_email.html", {
+    return templates.TemplateResponse("waitlist_thanks.html", {
         "request": request,
         "email": email
     })
